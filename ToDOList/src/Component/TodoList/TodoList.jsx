12ef08739f9e44092c12febdc561a91a79717358
BholaCraft07 <bholakumar1990kp@gmail.com>
2025-01-03 11:08:01 +0530

    Write all callback function above the return

--- ToDOList/src/Component/TodoList/TodoList.jsx
+++ ToDOList/src/Component/TodoList/TodoList.jsx
@@ -4,6 +4,30 @@ import TodoContext from "../../context/TodoContext";
 
 function TodoList() {
     const { list, setList } = useContext(TodoContext);
+    function onFinish(todo,isFinished){
+        const updatedlist = list.map((item) => {
+            if (item.id == todo.id) {
+                todo.finished = isFinished;
+            }
+            return item;
+        });
+        setList(updatedlist);
+    }
+    function onDelete(todo){
+        const updatedlist = list.filter((item) => item.id !== todo.id);
+        setList(updatedlist);
+    }
+    function onEdit(todoText,todo){
+        const updatedlist = list.filter((item) =>{
+            if(item.id === todo.id){
+                item.todoData =todoText
+            }
+            return item;
+        })
+        setList(updatedlist);
+    }
+
+
     return (
         <div className="">
             {list.length > 0 &&
@@ -12,28 +36,9 @@ function TodoList() {
                         key={todo.id}
                         todoData={todo.todoData}
                         isFinished={todo.finished}
-                        chnageFinished={(isFinished) => {
-                            const updatedlist = list.map((item) => {
-                                if (item.id == todo.id) {
-                                    todo.finished = isFinished;
-                                }
-                                return item;
-                            });
-                            setList(updatedlist);
-                        }}
-                        onDelete={()=>{
-                            const updatedlist = list.filter((item) => item.id !== todo.id);
-                            setList(updatedlist);
-                        }}
-                        onEdit={(todoText)=>{
-                            const updatedlist = list.filter((item) =>{
-                                if(item.id === todo.id){
-                                    item.todoData =todoText
-                                }
-                                return item;
-                            })
-                            setList(updatedlist);
-                        }}
+                        chnageFinished={(isFinished)=>onFinish(todo,isFinished)}
+                        onDelete={()=>onDelete(todo)}
+                        onEdit={(todoText)=>onEdit(todoText,todo)}
                         
                     />
                 ))}
